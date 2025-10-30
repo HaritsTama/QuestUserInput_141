6a79d94db2a9daf6d1fd204b7fcd47459b23c8ee
HaritsTama <nayotama1818@gmail.com>
2025-10-30 15:17:07 +0700

    menambahkan daftar gender dan field input nama lengkap

--- app/src/main/java/com/example/inputpraktikum/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/inputpraktikum/ui/theme/TeksEdit.kt
@@ -1,10 +1,17 @@
 package com.example.inputpraktikum.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 
 @Composable
@@ -19,4 +26,22 @@ fun FormDataDiri(modifier: Modifier
     var nama by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
+
+    val gender:List<String> = listOf("Laki-laki","Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = {Text(text = "Nama Lengkap")},
+            onValueChange = {
+                textNama = it
+            }
+        )
+    }
+
 }
