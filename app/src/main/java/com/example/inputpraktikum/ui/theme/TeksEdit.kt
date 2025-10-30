26747d1bb7b0d1af5e75f8c7d0a680ff4a13ae05
HaritsTama <nayotama1818@gmail.com>
2025-10-30 15:01:00 +0700

    menambahkan variable untuk menerima input dari keyboard

---
+++ app/src/main/java/com/example/inputpraktikum/ui/theme/TeksEdit.kt
@@ -0,0 +1,17 @@
+package com.example.inputpraktikum.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import java.lang.reflect.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+    //variable - variable untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+}
