eb3577eb968186bc36971bb9c7f41e2ac7d9416a
HaritsTama <nayotama1818@gmail.com>
2025-10-30 15:23:05 +0700

    Menambahkan opsi memilih jenis kelamin menggunakan RadioButton di dalam Row

--- app/src/main/java/com/example/inputpraktikum/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/inputpraktikum/ui/theme/TeksEdit.kt
@@ -2,8 +2,13 @@ package com.example.inputpraktikum.ui.theme
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,8 +16,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import java.lang.reflect.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -42,6 +47,21 @@ fun FormDataDiri(modifier: Modifier
                 textNama = it
             }
         )
+        Row {
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = { textJK = item}
+                ), verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        })
+                    Text(item)
+                }
+            }
+        }
     }
 
 }
