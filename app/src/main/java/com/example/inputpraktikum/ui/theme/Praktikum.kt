4bc8cc78a74118de8523eb7a69f644d3539cb835
HaritsTama <nayotama1818@gmail.com>
2025-11-01 13:53:15 +0700

    Menambahkan komponen input Alamat

--- app/src/main/java/com/example/inputpraktikum/ui/theme/Praktikum.kt
+++ app/src/main/java/com/example/inputpraktikum/ui/theme/Praktikum.kt
@@ -177,6 +177,29 @@ fun Praktikum(modifier: Modifier
                         }
 
                         Spacer(modifier = Modifier.height(20.dp))
+
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = "ALAMAT",
+                                fontSize = 12.sp,
+                                color = Color.Black,
+                                modifier = Modifier.padding(bottom = 8.dp)
+                            )
+                            OutlinedTextField(
+                                value = textAlamatP,
+                                singleLine = true,
+                                modifier = Modifier.fillMaxWidth(),
+                                label = { Text(text = "Isian nama lengkap")},
+                                onValueChange = {
+                                    textAlamatP = it
+                                }
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(28.dp))
                     }
                 }
             }
