e2bfbab6926b5306e8f8ec9e074e5f6d20b563fe
HaritsTama <nayotama1818@gmail.com>
2025-11-01 13:38:41 +0700

    Menambahkan state textNamaLengkap untuk input nama lengkap

--- app/src/main/java/com/example/inputpraktikum/MainActivity.kt
+++ app/src/main/java/com/example/inputpraktikum/MainActivity.kt
@@ -7,12 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.inputpraktikum.ui.theme.FormDataDiri
 import com.example.inputpraktikum.ui.theme.InputPraktikumTheme
+import com.example.inputpraktikum.ui.theme.Praktikum
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             InputPraktikumTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    FormDataDiri(
+                    Praktikum(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
