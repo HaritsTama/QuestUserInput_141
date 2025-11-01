3cb8e9dc01e6f1956974943b39613426a0866dc8
HaritsTama <nayotama1818@gmail.com>
2025-11-01 13:50:41 +0700

    Menambahkan komponen input Nama Lengkap menggunakan

--- app/src/main/java/com/example/inputpraktikum/ui/theme/Praktikum.kt
+++ app/src/main/java/com/example/inputpraktikum/ui/theme/Praktikum.kt
@@ -3,10 +3,13 @@ package com.example.inputpraktikum.ui.theme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -58,6 +62,46 @@ fun Praktikum(modifier: Modifier
                     fontWeight = FontWeight.Medium,
                     color = Color.White
                 )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(Color.White)
+                        .padding(24.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = "NAMA LENGKAP",
+                                fontSize = 12.sp,
+                                color = Color.Black,
+                                modifier = Modifier.padding(bottom = 8.dp)
+                            )
+                            OutlinedTextField(
+                                value = textNamaLengkap,
+                                singleLine = true,
+                                modifier = Modifier.fillMaxWidth(),
+                                label = { Text(text = "Isian nama lengkap") },
+                                onValueChange = {
+                                    textNamaLengkap = it
+                                }
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+                    }
+                }
             }
         }
     }
